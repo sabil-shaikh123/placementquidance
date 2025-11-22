b7ac6198e591e25b519b698fb1251d915a33a6a1
sabil-shaikh123 <sabilsheikh853@gmail.com>
2025-11-22 22:02:03 +0530

    Update PageController.java

--- compiler/compiler/src/main/java/com/project/controller/PageController.java
+++ compiler/compiler/src/main/java/com/project/controller/PageController.java
@@ -1,6 +1,9 @@
 package com.project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller  // For HTML pages only
 public class PageController {
