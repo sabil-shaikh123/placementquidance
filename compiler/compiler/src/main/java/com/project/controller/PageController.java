6b86ccc11ac7929d733a69f766dfccadd7bb49c6
sabil-shaikh123 <sabilsheikh853@gmail.com>
2026-01-16 19:52:31 +0530

    Update PageController.java

--- compiler/compiler/src/main/java/com/project/controller/PageController.java
+++ compiler/compiler/src/main/java/com/project/controller/PageController.java
@@ -20,7 +20,7 @@ public class PageController {
         return "login";
     }
     @GetMapping("/singin")
-    public String sing() {
+    public String singin() {
         // Spring Boot + Thymeleaf will render templates/home.html
         return "singup";
     }
