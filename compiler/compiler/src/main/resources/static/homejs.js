559d91917b645e9a3cb39b4da1057541f01f231f
sabil-shaikh123 <sabilsheikh853@gmail.com>
2026-01-16 19:10:11 +0530

    Update homejs.js

--- compiler/compiler/src/main/resources/static/homejs.js
+++ compiler/compiler/src/main/resources/static/homejs.js
@@ -1,27 +1,78 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   fetch("https://placementquidance-2.onrender.com/getAllCompany")
+//     .then(res => res.json())
+//     .then(data => {
+//       const div = document.getElementById("companyList");
+//       div.innerHTML = "";
+
+//       data.forEach(company => {
+//         const nameElement = document.createElement("p");
+//         nameElement.textContent = company.company_name;
+//         nameElement.style.cursor = "pointer";
+//         nameElement.style.color = "blue";
+//         nameElement.style.textDecoration = "underline";
+
+//         nameElement.onclick = () => {
+//           window.location.href = `/company?id=${company.company_id}`;
+//         };
+
+//         div.appendChild(nameElement);
+//       });
+//     })
+//     .catch(err => {
+//       console.error("Error fetching companies:", err);
+//     });
+// });
+
+
+// function taketest(){
+//     fetch("https://placementquidance-2.onrender.com/getAllCode")
+//     .then(res => res.json())
+//     .then(data => {
+//         const randomquestion = data[Math.floor(Math.random() * data.length)];
+//          window.location.href = `/compiler?id=${randomquestion.codeId}`;
+//     })
+// }
+
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("https://placementquidance-2.onrender.com/getAllCompany")
-    .then(res => res.json())
-    .then(data => {
-      const div = document.getElementById("companyList");
-      div.innerHTML = "";
 
-      data.forEach(company => {
-        const nameElement = document.createElement("p");
-        nameElement.textContent = company.company_name;
-        nameElement.style.cursor = "pointer";
-        nameElement.style.color = "blue";
-        nameElement.style.textDecoration = "underline";
+    //to check wheather the user is authenticated or not
+    //if user is not authenticated then that will return to the login page
+    const email = sessionStorage.getItem("Email");
+    if(email == null){
+        document.getElementById("authMsg").innerText ="You are not an authenticated user";
+         setTimeout(() => {
+            window.location.href = "login.html";
+         }, 2000);
 
-        nameElement.onclick = () => {
-          window.location.href = `/company?id=${company.company_id}`;
-        };
+    }else{
+        fetch("https://placementquidance-2.onrender.com/getAllCompany")
+            .then(res => res.json())
+            .then(data => {
+              const div = document.getElementById("companyList");
+              div.innerHTML = "";
 
-        div.appendChild(nameElement);
-      });
-    })
-    .catch(err => {
-      console.error("Error fetching companies:", err);
-    });
+              data.forEach(company => {
+                const nameElement = document.createElement("p");
+                nameElement.textContent = company.company_name;
+                nameElement.style.cursor = "pointer";
+                nameElement.style.color = "blue";
+                nameElement.style.textDecoration = "underline";
+
+                nameElement.onclick = () => {
+                  window.location.href = `company.html?id=${company.company_id}`;
+                };
+
+                div.appendChild(nameElement);
+              });
+            })
+            .catch(err => {
+              console.error("Error fetching companies:", err);
+            });
+    }
+
+
+
 });
 
 
@@ -30,9 +81,15 @@ function taketest(){
     .then(res => res.json())
     .then(data => {
         const randomquestion = data[Math.floor(Math.random() * data.length)];
-         window.location.href = `/compiler?id=${randomquestion.codeId}`;
+        window.location.href = `compiler.html?id=${randomquestion.codeId}`;
     })
 }
+
+//when the profile button is clicked to jump to profile page
+function goToProfile() {
+    window.location.href = "profile.html";
+}
+
 
 
 
