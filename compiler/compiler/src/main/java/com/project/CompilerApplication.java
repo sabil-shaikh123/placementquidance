31f9ce7555ccc82e7851fc6893ecf3a791b472d9
sabil-shaikh123 <sabilsheikh853@gmail.com>
2026-01-16 18:59:45 +0530

    Update CompilerApplication.java

--- compiler/compiler/src/main/java/com/project/CompilerApplication.java
+++ compiler/compiler/src/main/java/com/project/CompilerApplication.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 import java.util.List;
 
 @SpringBootApplication
+@EnableScheduling
 public class CompilerApplication implements CommandLineRunner {
 	@Autowired
 	private CompanyService companyService;
